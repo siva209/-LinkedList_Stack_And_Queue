0456c0cd478723f454aaa8fea998214d386b26e1
siva209 <sivanarayanayeruva209@gmail.com>
2021-06-07 19:45:56 +0530

    Added:Implementation of add method of linked list

--- src/main/java/com/java/datastructures/MyLinkedList.java
+++ src/main/java/com/java/datastructures/MyLinkedList.java
@@ -1,11 +1,46 @@
 package com.java.datastructures;
 
-public class MyLinkedList<K> {
-	public static void main(String[] args) {
-		System.out.println("Welcome to  Linked List Problem");
-		MyNode<Integer> firstNode=new MyNode<Integer>(56);
-		MyNode<Integer> secondNode=new MyNode<Integer>(30);
-		MyNode<Integer> thirdNode=new MyNode<	Integer>(70);	
+public class MyLinkedList<E> {
+	public INode<E> head;
+	public INode<E> tail;
+	public MyLinkedList() {
+		this.head = null;
+		this.tail = null;
+	}
+	/**Usecase 2
+	 * Function adds elements to the linked list(head changes)
+	 * @param node
+	 */
+	public void add(INode<E> node) {
+		if(head == null) {
+			head = node;
+			tail = node;
+		}
+		else {
+			INode<E> tempNode = head;
+			tempNode = head;
+			head = node;
+			head.setNext(tempNode);	
+		}
+	}
+	public void printNodes() {
+		INode<E> tempNode = head;
+		while(tempNode.getNext() != null) {
+			System.out.print(tempNode.getKey() + "->");
+			tempNode = tempNode.getNext();
+		}	
+		System.out.print(tempNode.getKey());
+	}
+	public static void main(String[] args)
+	{
+		MyLinkedList<Integer> list = new MyLinkedList<Integer>();
+		MyNode<Integer> firstNode = new MyNode<Integer>(56);
+		MyNode<Integer> secondNode = new MyNode<Integer>(30);
+		MyNode<Integer> thirdNode = new MyNode<Integer>(70);
+		list.add(thirdNode);
+		list.add(secondNode);
+		list.add(firstNode);
+		list.printNodes();
 	}
 }
 
